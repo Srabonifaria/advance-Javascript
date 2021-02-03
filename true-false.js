d157f36f06c7933d85e72ba3039d990e5d253327
Srabonifaria <76752669+Srabonifaria@users.noreply.github.com>
2021-02-04 01:05:32 +0600

    true vs false

---
+++ true-false.js
@@ -0,0 +1,19 @@
+// falsy...
+// 0;
+// '';
+// undefiend;
+// null;
+// NaN;
+// truthy...
+// '0';
+// ' ';
+
+// const name = '0';
+let name =12;
+console.log(name);
+if(name || name == 0){
+    console.log('condition is true');
+}
+else{
+    console.log('condition is false');
+}
