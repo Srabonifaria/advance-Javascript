334a826e139e5671150506e3bebc0168b409ef9f
Srabonifaria <76752669+Srabonifaria@users.noreply.github.com>
2021-02-04 14:57:34 +0600

    nul vs undefined clear for interview

---
+++ null-vs-undefined.js
@@ -0,0 +1,17 @@
+let pakhi;
+function add(num1, num2){
+    console.log(num1 + num2);
+    return;
+}
+
+function add(num1, num2){
+     console.log(num1 , num2);
+}
+const premik ={name:"smart dude",phone:08645678};
+console.log(premik.gf);
+let fun = undefined;
+console.log(fun);
+null
+
+let ages=[3,4,5];
+console.log(ages[11]);
